860390a229b434ffcf68917f52e1ee51c6758179
Lior Patael <74459571+LiorPatael@users.noreply.github.com>
2023-05-18 09:58:08 +0300

    Update buy-tickets.component.ts

--- src/app/buy-tickets/buy-tickets.component.ts
+++ src/app/buy-tickets/buy-tickets.component.ts
@@ -16,7 +16,7 @@ export class buy_tickets{
 
 export class BuyTicketsComponent {
   buy_tickets=[
-    {date:new Date(),phone:'052-1234567',details:'מחפש כרטיס למשחק נגד הצהבת', flag:true},
+    {date:new Date(),phone:'052-1234567',details:'מחפש כרטיס למשחק נגד מכבי תל אביב', flag:true},
     {date:new Date(),phone:'052-7654321',details:'מחפש כרטיס למשחק נגד הפועל באר שבע ליציע הדרומי', flag:true},
     {date:new Date(),phone:'052-3456789',details:'מחפש כרטיס למשחק נגד מכבי נתניה בכל יציע', flag:true},
     {date:new Date(),phone:'052-6543774',details:'מחפש כרטיס למשחק נגד הצהבת מוכן להציע יותר מעלות', flag:true}
